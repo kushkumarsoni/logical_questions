44229b34b7ad387e82d6b658db6458596025841e
Kush Kumar Soni <59538491+kushkumarsoni@users.noreply.github.com>
2023-07-20 10:51:08 +0530

    Update first_page.php

--- first_page.php
+++ first_page.php
@@ -1,4 +1,4 @@
-Please find the pair of given array in php
+1.) Please find the pair of given array in php
 <?php
 $array = [a,b,c,d];
 $data = array();
@@ -11,4 +11,29 @@ for($i=0;$i<count($array);$i++){
 
 echo "<pre>";
 print_r($data);
+?> 
+
+2.) Please print this pattern using php
+<?php
+function printPattern($rows)
+{
+    $currentChar = 'A';
+    
+    for ($i = 1; $i <= $rows; $i++) {
+        // Print spaces for the left-aligned pyramid
+        for ($space = 1; $space <= ($rows - $i); $space++) {
+            echo ' ';
+        }
+        
+        // Print characters in each row
+        for ($j = 1; $j <= $i; $j++) {
+            echo $currentChar . ' ';
+            $currentChar++;
+        }
+        
+        echo "<br/>"; // Move to the next line after each row is printed
+    }
+}
+
+printPattern(5);
 ?> 
